d4a203b917a6f12ced418381cb6893944b580e24
Marcos Peres <marcos.peres72@gmail.com>
2018-03-21 14:34:43 +0100

    Programming Assignment 2
    This is the second programm assignment of the Introduction to R course from Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## These function creates a list with 4 functions to load a Matrix,
+## read these Matrix, load the inverse of a  Matrix and read the
+## inverse of a Matrix in and from the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## These function receives a variable that contains a Matrix, check if
+## the inverse of this variable already exists in the cache. If the 
+## inverse exists, read this value from cache and show a message.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached inverse")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
